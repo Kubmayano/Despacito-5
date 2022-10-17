6e91f0f67c3db0aa2cac42da83853b12548f7147
Kubmayano <elliot.cornier@outlook.com>
2022-10-17 18:57:44 +0200

    Updated

---
+++ commands/goodman.js
@@ -0,0 +1,14 @@
+module.exports = {
+    meta: {
+        operatorOnly: false,
+        serverOnly: false,
+        description: "its good,,, man",
+        alias: null
+    },
+
+    execute (client, message, args) {
+
+        message.channel.send("https://cdn.discordapp.com/attachments/1003806879312527361/1028891579232043018/saul_sporadic.webm")
+
+    }
+}
