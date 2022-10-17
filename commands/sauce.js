5b99a7b592d3b31278bd090c7a5e8ee5642b17e0
Kubmayano <elliot.cornier@outlook.com>
2022-10-17 18:50:45 +0200

    sauce code

---
+++ commands/sauce.js
@@ -0,0 +1,15 @@
+module.exports = {
+    meta: {
+        operatorOnly: false,
+        serverOnly: false,
+        description: "sauce code for sauce :33",
+        alias: null
+    },
+
+    execute (client, message, args) {
+
+        const fs = require("fs")
+        const sauce = fs.readFileSync("./commands/sauce.js", "utf-8")
+        message.channel.send(`\`\`\`js\n${sauce.toString()} \`\`\``)
+    }
+}
