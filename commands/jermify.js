74abefd1c4de15540a9839954b4d19c0034d8b77
Kubmayano <elliot.cornier@outlook.com>
2022-10-17 16:56:53 +0200

    Update

--- commands/jermify.js
+++ commands/jermify.js
@@ -3,12 +3,24 @@ module.exports = {
     meta: {
         operatorOnly: true,
         serverOnly: false,
-        description: "Reloads your stupid ass commands 5head",
+        description: "Jerma laughs at your goofy ah ah ass",
         alias: null
     },
 
-    execute (client, message, args) {
-        const canvas = require('canvas')
+    async execute (client, message, args) {
+        
+        const { createCanvas, loadImage } = require('canvas')
+        const fs = require('fs')
+        const canvas = createCanvas(200, 200)
+        const ctx = canvas.getContext('2d')
+
+        const pfp = await loadImage(message.author.displayAvatarURL())
+        ctx.drawImage(pfp, 0, 0, 200, 200)
+
+        const buffer = canvas.toBuffer("image/png")
+        fs.writeFileSync(`./Files/Images/${message.author.id}.png`, buffer)
+        
+
         
     }
 }
